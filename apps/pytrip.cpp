2eda56ebe1f308bbaa683c107f1e6fe443b2f0d6
Oliver Schönrock <oliver@openbrackets.net>
2019-12-30 22:18:48 +0000

    finished fiddling with example make rebuild.sh more useful so you can easily switch between different installed compilers

--- apps/pytrip.cpp
+++ apps/pytrip.cpp
@@ -6,18 +6,11 @@ inline bool is_pythag_multiple(int x, int y, int z) noexcept { return std::gcd(s
 inline bool is_pythag_triple(int x, int y, int z) noexcept { return x * x + y * y == z * z; }
 
 template <typename F>
-inline void find_pythag_triples(int n, F&& action) {
-  int count = 0;
-  for (int z = 1;; ++z) {
-    for (int x = 1; x != z + 1; ++x) {
-      for (int y = x; y != z + 1; ++y) {
-        if (is_pythag_triple(x, y, z) && !is_pythag_multiple(x, y, z)) {
-          action(x, y, z);
-          if (++count == n) return;
-        }
-      }
-    }
-  }
+inline void find_pythag_triples(F&& process_triple) {
+  for (int z = 1;; ++z)
+    for (int x = 1; x != z + 1; ++x)
+      for (int y = x; y != z + 1; ++y)
+        if (is_pythag_triple(x, y, z) && !is_pythag_multiple(x, y, z) && !process_triple(x, y, z)) return;
 }
 
 int main(int argc, char* argv[]) {
@@ -33,5 +26,8 @@ int main(int argc, char* argv[]) {
 
   std::cout << "Finding top " << n << " Pythagorean triples\n";
 
-  find_pythag_triples(n, [](int x, int y, int z) { std::cout << '(' << x << ',' << y << ',' << z << ')' << '\n'; });
+  find_pythag_triples([&n](int x, int y, int z) {
+    std::cout << '(' << x << ',' << y << ',' << z << ')' << '\n';
+    return --n;
+  });
 }
--- rebuild.sh
+++ rebuild.sh
@@ -1,18 +1,40 @@
 #!/usr/bin/env bash
 
-pwd = "$(pwd)"
-base = "$(realpath $0)"
+pwd="$(pwd)"
+base="$(dirname $(realpath $0))"
+
+profile=${1:-default}
+
+conan_profile="$HOME/.conan/profiles/$profile"
+if [[ ! -e "$conan_profile" ]]
+then
+    echo "Usage: $0 [conan_profile]"
+    echo
+    echo "conan profile '$conan_profile' does not exist"
+    echo
+    echo "possible profiles:"
+    for p in $(ls "$HOME/.conan/profiles/")
+    do
+        basename "$p"
+    done
+    exit 1
+fi
+
+echo "using profile $conan_profile"
+
+eval "$(cat "$conan_profile" | egrep '^CC|CXX=.*$')"
+
 
 cd "$base"
-rm -rf build &&                                 \
-    mkdir build &&                              \
-    cd build &&                                 \
-    conan install .. --profile=clang &&         \
-    cd .. &&                                    \
-    cmake -DCMAKE_CXX_COMPILER=/usr/bin/clang++ \
-          -DCMAKE_C_COMPILER=/usr/bin/clang     \
-          -DCMAKE_EXPORT_COMPILE_COMMANDS=1     \
-          -B build -S .  &&                     \
+rm -rf build &&                                                \
+    mkdir build &&                                             \
+    cd build &&                                                \
+    conan install .. --profile="$conan_profile" &&             \
+    cd .. &&                                                   \
+    cmake -DCMAKE_CXX_COMPILER="$CXX"                          \
+          -DCMAKE_C_COMPILER="$CC"                             \
+          -DCMAKE_EXPORT_COMPILE_COMMANDS=1                    \
+          -B build -S .  &&                                    \
     cmake --build build
 
 cd "$pwd"
