00f92ced899609b46bf1463fe8328542820021f5
Oliver Schönrock <oliver@openbrackets.net>
2019-12-30 16:15:24 +0000

    clean version

--- apps/pytrip.cpp
+++ apps/pytrip.cpp
@@ -1,33 +1,9 @@
-#include <cstdlib>
 #include <iostream>
-#include <tuple>
-#include <vector>
+#include <numeric>
 
-using triple = std::tuple<int, int, int>;
+inline bool is_pythag_multiple(int x, int y, int z) noexcept { return std::gcd(std::gcd(x, y), std::gcd(x, z)) > 1; }
 
-// http://realtimecollisiondetection.net/blog/?p=89
-bool equal(double x, double y) {
-  return std::abs(x - y) <= std::numeric_limits<double>::epsilon() * std::max(1.0, std::max(std::abs(x), std::abs(y)));
-}
-
-bool are_multiples(triple a, triple b) {
-  auto [xa, ya, za] = a;
-  auto [xb, yb, zb] = b;
-  auto rat_x = static_cast<double>(xa) / xb;
-  auto rat_y = static_cast<double>(ya) / yb;
-  auto rat_z = static_cast<double>(za) / zb;
-  return
-      equal(rat_x, rat_y) &&
-      equal(rat_x, rat_z) &&
-      equal(rat_y, rat_z);
-}
-
-bool exists(const std::vector<triple>& existing, triple n) {
-  for (auto t: existing) {
-    if (are_multiples(t, n)) return true;
-  }
-  return false;
-}
+inline bool is_pythag_triple(int x, int y, int z) noexcept { return x * x + y * y == z * z; }
 
 int main(int argc, char* argv[]) {
   auto name = std::string{};
@@ -43,19 +19,13 @@ int main(int argc, char* argv[]) {
 
   std::cout << "Finding top " << n << " Pythagorean triples\n";
 
-  auto triples = std::vector<triple>{};
-
   int count = 0;
   for (int z = 1;; ++z) {
     for (int x = 1; x != z + 1; ++x) {
       for (int y = x; y != z + 1; ++y) {
-        if (x * x + y * y == z * z) {
-          auto t = std::make_tuple(x,y,z);
-          if (!exists(triples, t)) {
-            triples.push_back(t);
+        if (is_pythag_triple(x,y,z) && !is_pythag_multiple(x, y, z)) {
             std::cout << '(' << x << ',' << y << ',' << z << ')' << '\n';
             if (++count == n) return EXIT_SUCCESS;
-          }
         }
       }
     }
